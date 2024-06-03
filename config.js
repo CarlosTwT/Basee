bee95a151378629b96f11639ea73ddf1083b80a4
ᴀꜱᴛʀᴏ ʟᴇɢᴇɴᴅ <astrolegend0010@gmail.com>
2024-06-03 20:50:39 +0100

    doe

--- config.js
+++ config.js
@@ -1,15 +1,17 @@
 const fs = require('fs');
 const chalk = require('chalk');
 
+
+// Other
 global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
 global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "𝗔𝘀𝘁𝗿𝗼 𝗟𝗲𝗴𝗲𝗻𝗱";
-global.owner = ['2349027862116'];
-global.premium = ['2349027862116'];
+global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
+global.owner = ['917050906659'];
+global.premium = ['917050906659'];
 global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
-global.author = "ᴍᴜʟᴛɪʙᴏᴛᴛᴏ ᴡʜᴀᴛꜱᴀᴘᴘ";
+global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
 global.sessionName = 'session';
 global.prefa = [".", "!", "/", "?"];
 global.typemenu = "v1"
@@ -24,20 +26,20 @@ global.onlygroup = false
 global.onlypc = false
 global.welcome = true
 global.autoBlock = true
-global.version = "ᴠ1.0.1"
+ 
 /*REPLY MESSAGE*/
 global.mess = {
-    banned: '`ʏᴏᴜ ʜᴀᴠᴇ ʙᴇᴇɴ ʙᴀɴɴᴇᴅ ꜰʀᴏᴍ ᴜꜱɪɴɢ ᴄᴏᴍᴍᴀɴᴅꜱ. ⚠️`',
-    bangc: '`ᴍᴇᴍʙᴇʀꜱ ɪɴ ᴛʜɪꜱ ɢʀᴏᴜᴘ ᴀʀᴇ ʙᴀɴɴᴇᴅ ꜰʀᴏᴍ ᴜꜱɪɴɢ ᴄᴏᴍᴍᴀɴᴅꜱ. ⚠️`',
-    success: '`ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟ ✓`',
-    admin: '`ᴏɴʟʏ ᴀᴅᴍɪɴꜱ ᴄᴀɴ ʀᴇQᴜᴇꜱᴛ ᴇxᴇᴄᴜᴛɪᴏɴ ᴏꜰ ᴛʜɪꜱ ᴄᴏᴍᴍᴀɴᴅ. ⚠️`',
-    botAdmin: '`ᴍᴀᴋᴇ ᴍᴇ ᴀɴ ᴀᴅᴍɪɴ ᴛᴏ ᴇxᴇᴄᴜᴛᴇ ᴛʜɪꜱ ʀᴇQᴜᴇꜱᴛ ꜱɪʀ. 🐔`',
-    owner: '`ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴍʏ ᴏᴡɴᴇʀ. 😐`',
-    group: '`*ꜰᴀɪʟᴇᴅ:* ᴄᴏᴍᴍᴀɴᴅ ᴡᴀꜱ ᴍᴀᴅᴇ ꜰᴏʀ ɢʀᴏᴜᴘꜱ ᴏɴʟʏ.`',
-    private: '`*ꜰᴀɪʟᴇᴅ:* ᴄᴏᴍᴍᴀɴᴅ ᴡᴀꜱ ᴍᴀᴅᴇ ꜰᴏʀ ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀᴛꜱ ᴏɴʟʏ.`',
-    bot: '`ꜰᴏʀ ꜱᴜᴅᴏ ɴᴜᴍʙᴇʀꜱ ᴏɴʟʏ.`',
-    wait: '🔃 ᴘʀᴏᴄᴇꜱꜱɪɴɢ ʀᴇQᴜᴇꜱᴛ...',
-    endLimit: '`ʏᴏᴜʀ ᴅᴀɪʟʏ ʟɪᴍɪᴛ ʜᴀꜱ ᴇxᴘɪʀᴇᴅ, ᴛʜᴇ ʟɪᴍɪᴛ ᴡɪʟʟ ʙᴇ ʀᴇꜱᴇᴛ ᴇᴠᴇʀʏ 12 ʜᴏᴜʀꜱ.`',
+    banned: '`You are *Banned* fron using commands!`',
+    bangc: '`This Group is *Banned* from using Commands!`',
+    success: '`✓ Success`',
+    admin: '`This feature is only for group admins`',
+    botAdmin: '`I am not an admin!`',
+    owner: '`You are not my owner`',
+    group: '`You can use this command only in groups ❌`',
+    private: '`Feature is used only for private chats!`',
+    bot: '`Bot number user special features`',
+    wait: '*Processing Your request*',
+    endLimit: '`Your daily limit has expired, the limit will be reset every 12 hours`',
 };
 
 global.limitawal = {
